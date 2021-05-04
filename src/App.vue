830e1cd07db5beb577994eee65ae5df382a48eca
fither <onur.hamarattt@gmail.com>
2021-05-04 11:39:28 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Search v-on:searchRequested="handleSearch"/>
+    <Preview :isLoading="isLoading" :gifs="fetchedGifs" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search'
+import Preview from './components/Preview';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      giphyTrendingAPI: 'https://api.giphy.com/v1/gifs/trending?api_key=hOh3MHTITlzyC8t1oPhESg9kb4xd3Bkc&limit=25&rating=g',
+      giphySearchAPI: 'https://api.giphy.com/v1/gifs/search?api_key=hOh3MHTITlzyC8t1oPhESg9kb4xd3Bkc&limit=25&offset=0&rating=g&lang=en&q=',
+      fetchedGifs: [],
+      isLoading: true
+    }
+  },
   components: {
-    HelloWorld
+    Search,
+    Preview
+  },
+  created() {
+    fetch(this.giphyTrendingAPI)
+      .then(res => { return res.json(); })
+      .then(res => { this.fetchedGifs = res.data; this.isLoading = false })
+  },
+  methods: {
+    handleSearch(query) {
+      this.isLoading = true;
+      this.fetchedGifs = [];
+      fetch(this.giphySearchAPI + query)
+        .then(res => { return res.json(); })
+        .then(res => { this.fetchedGifs = res.data; this.isLoading = false})
+    }
   }
 }
 </script>
@@ -19,8 +45,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Preview.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="preview">
+    <h1 v-if="isLoading">Loading...</h1>
+    <ul v-if="!isLoading">
+      <li v-for="(gif, index) in gifs" :key="index">
+        <img :src="gif.images.fixed_height.url" alt="">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'gifs', 'isLoading' ]
+}
+</script>
+
+<style scoped>
+  .preview {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  li {
+    list-style: none;
+    float: left;
+    margin: 20px;
+  }
+
+</style>
